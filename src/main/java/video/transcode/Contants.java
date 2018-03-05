079e7a9c2897e4081e7885ef757a30b447de6cfa
王路遥 <wangluyao@jf180.cn>
2018-03-05 15:52:05 +0800

    嘿嘿嘿

---
+++ src/main/java/video/transcode/Contants.java
@@ -0,0 +1,18 @@
+package video.transcode;
+
+public class Contants {
+	public static final String ffmpegpath
+			= "D:\\video transcoding\\ffmpeg.exe";//ffmpeg的安装位置
+	public static final String mencoderpath
+			= "D:\\video transcoding"; // mencoder的目录
+
+	public static final String videofolder
+			= "C:\\Users\\Administrator\\Desktop\\video"; // 需要被转换格式的视频目录
+	public static final String targetfolder
+			= "C:\\Users\\Administrator\\Desktop\\testVideo\\"; // 转换后视频的目录
+
+	public static final String videoRealPath
+			= "C:\\Users\\Administrator\\Desktop\\video"; // 需要被截图的视频目录;
+	public static final String imageRealPath
+			= "D:\\tools\\imgs\\"; // 截图的存放目录
+}
